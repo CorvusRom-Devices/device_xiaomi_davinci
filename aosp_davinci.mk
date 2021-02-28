8c86d056a80a4844445560c1110d905f7446b347
CoolGuyMK <coolguymk1@gmail.com>
2021-02-28 13:40:27 +0200

    Inherit miuicamera

--- aosp_davinci.mk
+++ aosp_davinci.mk
@@ -17,6 +17,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product-if-exists, vendor/miuicamera/common/common-vendor.mk)
 
 # Inherit from davinci device
 $(call inherit-product, device/xiaomi/davinci/device.mk)
