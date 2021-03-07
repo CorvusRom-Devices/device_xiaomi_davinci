ed4d81b852c92510d396c95ea263b308d3fcf34d
Laker <lakerman1987@gmail.com>
2021-03-07 21:19:50 +0200

    parts: Add vibration control

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -29,6 +29,7 @@ import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.utils.FileUtils;
 import org.lineageos.settings.display.KcalUtils;
+import org.lineageos.settings.vibrator.VibratorSettings;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -44,6 +45,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.startService(context);
+       VibratorSettings.restoreValue(context);
 
         if (KcalUtils.isKcalSupported())
             KcalUtils.writeCurrentSettings(sharedPrefs);
